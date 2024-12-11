543b13a1eb85270c2d805a7eca9ce9bc5c0a3db1
kokos <tumashov.05@bk.ru>
2024-12-12 06:04:19 +0700

    error

--- Dec.cpp
+++ Dec.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include <clocale>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <regex>
-#include <map>
-#include <algorithm>
+#include <iostream> 
+#include <clocale> 
+#include <string> 
+#include <fstream> 
+#include <vector> 
+#include <regex> 
+#include <unordered_map> 
+#include <algorithm> 
 using namespace std;
 
 struct FileInfo {
@@ -21,8 +21,6 @@ string getFileDate(string s);
 int getFileSum(string s);
 
 int main() {
-    int a = 0;
-    int b = 1;
     ifstream ist("file.txt");
     if (!ist.is_open()) {
         cerr << "Error opening file" << endl;
@@ -35,14 +33,14 @@ int main() {
         files.push_back({ getFileName(s), getFileDate(s), getFileSum(s) });
     }
 
-    // Вывод исходного списка
+    // Вывод исходного списка 
     cout << "Original list:\n";
     for (const auto& item : files) {
         cout << item << "\n";
     }
 
-    // Сортировка по дате
-    sort(files.begin(), files.end(), [](const FileInfo& a, const FileInfo& b) {
+    // Сортировка по дате 
+    stable_sort(files.begin(), files.end(), [](const FileInfo& a, const FileInfo& b) {
         return a.date < b.date;
         });
 
@@ -51,21 +49,21 @@ int main() {
         cout << item << "\n";
     }
 
-    // Суммирование доходов по имени
-    map<string, int> incomeSummary;
+    // Суммирование доходов по имени с использованием unordered_map 
+    unordered_map<string, int> incomeSummary;
     for (const auto& item : files) {
         incomeSummary[item.name] += item.sum;
     }
 
-    // Нахождение топ-3 доходов
+    // Нахождение топ-3 доходов 
     vector<pair<string, int>> sortedIncome(incomeSummary.begin(), incomeSummary.end());
-    sort(sortedIncome.begin(), sortedIncome.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
-        return b.second < a.second; // По убыванию суммы
+    nth_element(sortedIncome.begin(), sortedIncome.begin() + 3, sortedIncome.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+        return b.second > a.second;
         });
 
     cout << "\nTop-3 incomes:\n";
-    for (size_t i = 0; i < min(sortedIncome.size(), size_t(3)); ++i) {
-        cout << sortedIncome[i].first << ": " << sortedIncome[i].second << "\n";
+    for (size_t i = 0; i < min(size_t(3), sortedIncome.size()); ++i) {
+        cout << sortedIncome[i].first << ": " << sortedIncome[i].second << " bytes\n";
     }
 
     return 0;
@@ -107,8 +105,8 @@ vector<string> extractData(const string& data, const string& regex) {
 }
 
 std::ostream& operator<<(std::ostream& stream, const FileInfo& fileInfo) {
-    stream << "Name: " << fileInfo.name;
+    stream << "File Name: " << fileInfo.name;
     stream << ", Date: " << fileInfo.date;
-    stream << ", Sum: " << fileInfo.sum;
+    stream << ", Sum: " << fileInfo.sum << " bytes";
     return stream;
 }
